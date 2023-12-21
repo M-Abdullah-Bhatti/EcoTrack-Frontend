1ac68b0af68b9ceaaa1038f6b80b4a497d45f010
M-Abdullah-Bhatti <muhammadabdullahimdad@gmail.com>
2023-12-21 11:06:46 +0500

    logout added

--- src/screen/Home/CommunityScreen.jsx
+++ src/screen/Home/CommunityScreen.jsx
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity } from "react-native";
+import React from "react";
+import { useDispatch } from "react-redux";
+import { logout } from "../../redux/userSlice";
 
-const CommunityScreen = () => {
+const CommunityScreen = ({ navigation }) => {
+  const dispatch = useDispatch();
+  function handleLogout() {
+    dispatch(logout());
+    navigation.replace("AuthNavigation");
+  }
+
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-      <Text style={{fontSize: 24, fontFamily: 'PoppinsMedium'}}>Community Coming Soon...</Text>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text style={{ fontSize: 24, fontFamily: "PoppinsMedium" }}>
+        Community Coming Soon...
+      </Text>
+      <TouchableOpacity
+        style={{
+          backgroundColor: "#46A667",
+          marginTop: 18,
+          paddingHorizontal: 12,
+          paddingVertical: 6,
+          borderRadius: 6,
+          alignItems: "center",
+        }}
+        onPress={handleLogout}
+      >
+        <Text style={{ color: "white" }}>Logout</Text>
+      </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
-export default CommunityScreen
+export default CommunityScreen;
