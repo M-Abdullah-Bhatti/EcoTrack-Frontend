7a2b3b1c6bfaab5b9fa4bf54f4cc80db2582013e
Abdullah Aziz <abdullahazees4@gmail.com>
2024-03-04 15:41:16 +0500

    new login screen added

--- src/screen/Auth/LoginScreen.jsx
+++ src/screen/Auth/LoginScreen.jsx
@@ -13,9 +13,11 @@ import {
   Animated,
   Easing,
   Dimensions,
+  KeyboardAvoidingView,
 } from "react-native";
 import React, { useState } from "react";
-import { SafeAreaView, KeyboardAvoidingView } from "react-native";
+import { SafeAreaView } from "react-native";
+import { FontAwesome, AntDesign, Feather } from "@expo/vector-icons";
 import { useDispatch } from "react-redux";
 import Svg, { Path, Rect } from "react-native-svg";
 import { loginStart, loginSuccess } from "../../redux/userSlice";
@@ -45,10 +47,21 @@ const Login = ({ navigation }) => {
   const imgSrc = require("../../../assets/logo-text.png");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordVisible, setPasswordVisible] = useState(false);
   const { width, height } = Dimensions.get("screen");
+  const [errors, setErrors] = useState({});
   const dispatch = useDispatch();
   const AnimatedRect = Animated.createAnimatedComponent(Rect);
   const animatedVal = React.useRef(new Animated.Value(0)).current;
+  const validateForm = () => {
+    let errors = {};
+    if (!email) errors.email = "Email is required";
+    if (!password) errors.password = "Password is required";
+
+    setErrors(errors);
+
+    return Object.keys(errors).length === 0;
+  };
 
   const chartWidth = width;
   const chartHeight = 225;
@@ -71,9 +84,12 @@ const Login = ({ navigation }) => {
     }).start();
   }, []);
   const handleLogin = async () => {
-    if (!email || !password) {
-      alert("Please enter both email and password");
-      return;
+    // if (!email || !password) {
+    //   alert("Please enter both email and password");
+    //   return;
+    // }
+    if (!validateForm()) {
+      return; // Exit if the form is not valid
     }
 
     try {
@@ -108,7 +124,6 @@ const Login = ({ navigation }) => {
 
   return (
     <KeyboardAvoidingView
-      behavior={Platform.OS === "ios" ? "padding" : "height"}
       keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 0}
       style={{ flex: 1 }}
     >
@@ -152,24 +167,133 @@ const Login = ({ navigation }) => {
         </View>
 
         <View style={{ alignItems: "center", justifyContent: "center" }}>
-          <Image source={imgSrc} height={47} style={{ marginTop: 0 }} />
+          <Text
+            style={{
+              fontSize: 28,
+              color: "#04753E",
+              fontWeight: "bold",
+              marginBottom: 6,
+              marginTop: -10,
+            }}
+          >
+            Eco Track
+          </Text>
+          <Text style={{ fontSize: 14, color: "black", marginBottom: 5 }}>
+            Login To your Account
+          </Text>
         </View>
-        <View>
-          <TextInput
-            style={styles.input}
-            onChangeText={(value) => setEmail(value)}
-            value={email}
-            placeholder="Enter Your Email..."
-          />
-          <TextInput
-            style={styles.input}
-            onChangeText={(value) => setPassword(value)}
-            value={password}
-            placeholder="Enter Your Password..."
-            secureTextEntry={true}
-          />
+        <View style={{ width: "100%", display: "flex", alignItems: "center" }}>
+          {errors.email && (
+            <View style={{ width: "90%" }}>
+              <Text
+                style={{
+                  color: "red",
+
+                  marginBottom: -10,
+                  fontSize: 12,
+                  marginTop: 10,
+                  textAlign: "left",
+                }}
+              >
+                {errors.email}
+              </Text>
+            </View>
+          )}
+          <View
+            style={[
+              styles.inputContainer,
+              errors.email && { borderColor: "red", borderWidth: 1 },
+            ]}
+          >
+            <FontAwesome
+              name="user"
+              size={20}
+              style={{ marginLeft: 7 }}
+              color="#04753E"
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={(value) => setEmail(value)}
+              value={email}
+              placeholder="Enter Your Email..."
+              placeholderTextColor="#04753E"
+            />
+          </View>
+          {errors.password && (
+            <View style={{ width: "90%" }}>
+              <Text
+                style={{
+                  color: "red",
+
+                  marginBottom: -10,
+                  marginTop: 10,
+                  textAlign: "left",
+                  fontSize: 12,
+                }}
+              >
+                {errors.password}
+              </Text>
+            </View>
+          )}
+
+          <View
+            style={[
+              styles.inputContainer,
+              errors.password && { borderColor: "red", borderWidth: 1 },
+            ]}
+          >
+            <FontAwesome
+              name="lock"
+              size={20}
+              style={{ marginLeft: 7 }}
+              color="#04753E"
+            />
+            <TextInput
+              style={styles.input}
+              onChangeText={(value) => setPassword(value)}
+              value={password}
+              placeholder="Enter Your Password..."
+              placeholderTextColor="#04753E"
+              secureTextEntry={!passwordVisible ? true : false}
+            />
+            {passwordVisible ? (
+              <TouchableOpacity
+                style={{
+                  marginLeft: -12,
+                }}
+                onPress={() => setPasswordVisible(false)}
+              >
+                <Feather name="eye-off" size={18} color="#04753E" />
+              </TouchableOpacity>
+            ) : (
+              <TouchableOpacity
+                style={{
+                  marginLeft: -12,
+                }}
+                onPress={() => setPasswordVisible(true)}
+              >
+                <AntDesign name="eye" size={18} color="#04753E" />
+              </TouchableOpacity>
+            )}
+          </View>
+
+          <TouchableOpacity style={{ width: "90%" }}>
+            <Text
+              style={{
+                textAlign: "right",
+
+                color: "#04753E",
+                fontWeight: "700",
+              }}
+            >
+              Forgot Password?
+            </Text>
+          </TouchableOpacity>
+
           <TouchableOpacity style={styles.loginBtn} onPress={handleLogin}>
-            <Text style={styles.loginTExt}>LOGIN</Text>
+            <Text style={styles.loginTExt} onPress={() => handleSubmit()}>
+              LOGIN
+            </Text>
           </TouchableOpacity>
         </View>
         <View style={styles.breakPoint}>
@@ -177,10 +301,16 @@ const Login = ({ navigation }) => {
           <Text style={styles.bpText}>OR</Text>
           <View style={styles.item}></View>
         </View>
-        <TouchableOpacity style={styles.googleLoginBtn}>
-          <Image source={require("../../../assets/googlelogo.png")} />
-          <Text style={styles.loginTExt}>CONTINUE WITH GOOGLE</Text>
-        </TouchableOpacity>
+        <View style={{ width: "100%", display: "flex", alignItems: "center" }}>
+          <TouchableOpacity style={styles.googleLoginBtn}>
+            <Image
+              style={{ width: 30, height: 30, objectFit: "cover" }}
+              source={require("../../../assets/googlelogo.png")}
+            />
+            <Text style={styles.googleloginTExt}>CONTINUE WITH GOOGLE</Text>
+          </TouchableOpacity>
+        </View>
+
         <View
           style={{
             display: "flex",
@@ -189,13 +319,13 @@ const Login = ({ navigation }) => {
             marginTop: 10,
           }}
         >
-          <Text style={{ fontSize: 18 }}>
+          <Text style={{ fontSize: 14, color: "rgba(0, 0, 0, 0.5)" }}>
             Don’t have an account?{" "}
             <Text
               onPress={() => navigation.navigate("SignUp")}
-              style={{ marginLeft: 3, color: "#096151", fontWeight: "700" }}
+              style={{ color: "#096151", fontWeight: "700" }}
             >
-              Login
+              Register
             </Text>
           </Text>
         </View>
@@ -217,25 +347,31 @@ const styles = StyleSheet.create({
     position: "relative",
   },
   input: {
-    height: 50,
+    height: "100%",
     margin: 12,
-    borderWidth: 1,
+
     padding: 10,
     borderRadius: 8,
     borderColor: "#acacac",
+    width: "82%",
+
+    fontSize: 12,
   },
   loginBtn: {
-    // width: 393,
-    height: 50,
+    width: "90%",
+
+    height: 45,
+
     margin: 12,
-    backgroundColor: "#2DBAA0",
-    borderRadius: 8,
+    backgroundColor: "#0B8337",
+    borderRadius: 28,
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
+    marginTop: 30,
   },
   loginTExt: {
-    fontSize: 18,
+    fontSize: 14,
     fontWeight: "bold",
     color: "white",
   },
@@ -250,28 +386,47 @@ const styles = StyleSheet.create({
   item: {
     width: "45%",
     backgroundColor: "black",
-    height: 1.5,
+    height: 1,
     shadowColor: "black",
     shadowOffset: 20,
   },
   bpText: {
-    fontSize: 20,
+    fontSize: 12,
     color: "black",
   },
   googleLoginBtn: {
-    height: 50,
+    height: 45,
     margin: 12,
-    backgroundColor: "#2DBAA0",
+    backgroundColor: "#EBF6EF",
     flexDirection: "row",
-    borderRadius: 8,
+    borderRadius: 28,
     display: "flex",
     alignItems: "center",
     justifyContent: "center",
     gap: 10,
+    width: "90%",
+    borderColor: "#04753E",
+    borderWidth: 0.5,
+  },
+  inputContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    margin: 12,
+    // borderWidth: 1,
+    borderRadius: 8,
+    borderColor: "#acacac",
+    height: 45,
+    width: "90%",
+    backgroundColor: "#EBF6EF",
   },
   svg: {
     position: "absolute",
     zIndex: 10,
+  },
+  googleloginTExt: {
+    fontSize: 14,
+    fontWeight: "bold",
+    color: "#04753E",
   },
 });
 
